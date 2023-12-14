1ca502dd2b7f1f4e6b350792bccc1f2de2514f7e
InDaviX <dszewczyk235@gmail.com>
2023-12-14 20:13:57 +0100

    Nie do końca łapie useState, ale spoko

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
+  const [text, setText] = useState('Cheloł łord!');
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <Text onPress={() => setText('Hello, World!')}>{text}</Text>
     </SafeAreaView>
   );
 };
