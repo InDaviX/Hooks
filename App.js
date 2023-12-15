f762a67a3a10bf632c948abdea18de912974f3fc
InDaviX <dszewczyk235@gmail.com>
2023-12-15 14:19:51 +0100

    Blank

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  const [text, setText] = useState(0);
-
-  useEffect(() => {
-    console.log('The text has changed', text);
-  }, [text]);
-
-  useEffect(() => {
-    console.log('Aplication started');
-  }, []);
-
   return (
     <SafeAreaView>
-      <Text onPress={() => setText(text + 1)}>{text}</Text>
+      <Text>Hello World!</Text>
     </SafeAreaView>
   );
 };
