990c1c48387774c4f044927efffe2dbc8a4a8dce
InDaviX <dszewczyk235@gmail.com>
2023-12-15 17:52:40 +0100

    Blank

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, Button, View} from 'react-native';
-import {ThemeContext} from './contexts/ThemeContext';
-import HomeScreen from './components/HomeScreen/HomeScreen';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
   return (
-    <ThemeContext.Provider value={isDarkMode}>
-      <SafeAreaView>
-        <HomeScreen />
-        <Button
-          title={'Switch Theme'}
-          onPress={toggleDarkMode}
-          style={{width: '50%'}}
-        />
-      </SafeAreaView>
-    </ThemeContext.Provider>
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
   );
 };
 
