0b50db57bc9058511b3590dfde7b47d32ab6a1bf
InDaviX <dszewczyk235@gmail.com>
2023-12-15 17:22:07 +0100

    Przewijanie do góry, działające

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, Text, ScrollView, Button} from 'react-native';
 
 const App = () => {
+  let array1 = Array(200).fill(0);
+  const scrollViewRef = useRef(null);
+  const click = () => {
+    scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
+  };
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <ScrollView ref={scrollViewRef}>
+        {array1.map((v, index) => (
+          <Text key={index}> {index + 1}. Hello World!</Text>
+        ))}
+        <Button onPress={click} title={'Scroll to top'} style={{margin: 200}} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
