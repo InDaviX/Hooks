c59009ca9ca7ea783c6987ed7bd28c139fd0dd9f
InDaviX <dszewczyk235@gmail.com>
2023-12-15 13:50:55 +0100

    blank

--- App.js
+++ App.js
@@ -1,53 +1,12 @@
-import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
-const LanguageSwitcher = () => {
-  const [currentLanguage, setCurrentLanguage] = useState('english');
-
-  const languages = {
-    english: 'Hello World!',
-    polish: 'Witaj Świecie!',
-    french: 'Bonjour le Monde!',
-    german: 'Hallo Welt!',
-    spanish: '¡Hola Mundo!',
-    italian: 'Ciao Mondo!',
-    russian: 'Привет, мир!',
-  };
-
-  const toggleLanguage = () => {
-    const languageKeys = Object.keys(languages);
-    const currentIndex = languageKeys.indexOf(currentLanguage);
-    const nextIndex = (currentIndex + 1) % languageKeys.length;
-    const nextLanguage = languageKeys[nextIndex];
-    setCurrentLanguage(nextLanguage);
-  };
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{languages[currentLanguage]}</Text>
-      <Button
-        title="Toggle Language"
-        onPress={toggleLanguage}
-        style={styles.button}
-      />
-    </View>
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-  },
-  text: {
-    fontSize: 20,
-    marginBottom: 16,
-  },
-  button: {
-    width: '50%',
-  },
-});
-
-export default LanguageSwitcher;
+export default App;
