e8e7b68f902431df03a5429324714d1bc74d80e9
InDaviX <dszewczyk235@gmail.com>
2023-12-15 14:17:52 +0100

    Wykrywanie zmian stanu

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
+  const [text, setText] = useState(0);
+
+  useEffect(() => {
+    console.log('The text has changed', text);
+  }, [text]);
+
+  useEffect(() => {
+    console.log('Aplication started');
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <Text onPress={() => setText(text + 1)}>{text}</Text>
     </SafeAreaView>
   );
 };
