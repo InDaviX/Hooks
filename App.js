67fc46447bf0c79b07e1b8421f0da6dda7635636
InDaviX <dszewczyk235@gmail.com>
2023-12-15 17:34:50 +0100

    Zmiana koloru tła i tekstu w jendym pliku

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {SafeAreaView, Text, Button, View} from 'react-native';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <View style={{backgroundColor: isDarkMode ? '#222222' : '#aaaaaa'}}>
+        <Text style={{color: isDarkMode ? '#FFFFFF' : '#000000'}}>
+          Hello World!
+        </Text>
+        <Button title={'Switch Theme'} onPress={toggleDarkMode} />
+      </View>
     </SafeAreaView>
   );
 };
