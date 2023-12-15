b98a8d37f99949b3a7b2ea183ab8f76907d62903
InDaviX <dszewczyk235@gmail.com>
2023-12-15 17:23:39 +0100

    Blank

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import React, {useRef} from 'react';
-import {SafeAreaView, Text, ScrollView, Button} from 'react-native';
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  let array1 = Array(200).fill(0);
-  const scrollViewRef = useRef(null);
-  const click = () => {
-    scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
-  };
   return (
     <SafeAreaView>
-      <ScrollView ref={scrollViewRef}>
-        {array1.map((v, index) => (
-          <Text key={index}> {index + 1}. Hello World!</Text>
-        ))}
-        <Button onPress={click} title={'Scroll to top'} style={{margin: 200}} />
-      </ScrollView>
+      <Text>Hello World!</Text>
     </SafeAreaView>
   );
 };
